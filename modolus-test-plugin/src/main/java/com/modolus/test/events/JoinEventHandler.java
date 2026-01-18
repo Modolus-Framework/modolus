6fb29ef19a31dab8397f547c0e26b53d591be15e
Louis <dev@louis-schmieder.de>
2026-01-18 20:38:01 +0100

    fix join event handler in test plugin

--- modolus-test-plugin/src/main/java/com/modolus/test/events/JoinEventHandler.java
+++ modolus-test-plugin/src/main/java/com/modolus/test/events/JoinEventHandler.java
@@ -1,5 +1,6 @@
 package com.modolus.test.events;
 
+import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.event.events.player.PlayerConnectEvent;
 import com.modolus.annotations.event.EventHandler;
 import com.modolus.annotations.event.EventListener;
@@ -7,16 +8,20 @@ import com.modolus.annotations.singleton.CreateOnRuntime;
 import com.modolus.annotations.singleton.InjectSingleton;
 import com.modolus.annotations.singleton.ProvideSingleton;
 import com.modolus.core.database.Database;
+import com.modolus.core.logger.Logger;
+import com.modolus.core.logger.LoggerUtils;
 import org.jetbrains.annotations.NotNull;
 
 @CreateOnRuntime
 @EventListener
 @InjectSingleton(Database.class)
-@ProvideSingleton(EventHandler.class)
+@ProvideSingleton(JoinEventHandler.class)
 public class JoinEventHandler extends AbstractJoinEventHandler {
 
     @EventHandler
     public void onPlayerConnect(final @NotNull PlayerConnectEvent event) {
+        event.getPlayerRef().sendMessage(Message.raw("Hello from the server!"));
+        LoggerUtils.printInfo(Logger.getPluginLogger(), String.format("Player joined %s", event.getPlayerRef().getUsername()));
     }
 
 }
